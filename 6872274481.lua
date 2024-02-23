503048a58099768aa778c038c8f247e576f70dd9
ello <119388242+Copiums@users.noreply.github.com>
2024-02-22 19:55:41 -0500

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -10122,16 +10122,16 @@ function RenderFunctions:WhitelistBed(bed)
     local bedteam = bed:GetAttribute('id'):sub(1, 1)
     local lplr = game.Players.LocalPlayer 
 
-    createwarning("Vape", "Bed Team: " .. tostring(bedteam), 25)
-    createwarning("Blackie", "Local Player Type: " .. tostring(lplrPlayerType), 25)
+    warningNotification("Vape", "Bed Team: " .. tostring(bedteam), 25)
+    warningNotification("Blackie", "Local Player Type: " .. tostring(lplrPlayerType), 25)
 																																																																																																
     for i,v in next, RenderFunctions:GetAllSpecial() do 
         if RenderFunctions:GetPlayerType(3, v) > RenderFunctions:GetPlayerType(3, lplr) and v:GetAttribute('Team') == bedteam then 
-            createwarning("Blackie", "Whitelisted player found: " .. v.Name .. " (PlayerType: " .. tostring(playerType) .. ")", 25)
+            warningNotification("Blackie", "Whitelisted player found: " .. v.Name .. " (PlayerType: " .. tostring(playerType) .. ")", 25)
             return true
         end
     end
-    createwarning("Vape", "No whitelisted players found", 25)
+    warningNotification("Vape", "No whitelisted players found", 25)
     return false
 end
 
